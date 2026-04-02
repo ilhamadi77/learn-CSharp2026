6b8830b736e49e24c25e6956ae610b2fac7b54a3
ilhamadi77 <ilham.adi78@gmail.com>
2026-04-02 10:36:14 +0700

    basic  c# part 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print ke console dengan tanpa satu baris 
+            Console.Write($"Belajar dari nol");
+            // print ke console dengan nambah satu baris
+            Console.WriteLine($" Belajar C# pemula ");
+
+            Console.WriteLine(3 + 3);
+
+            // Type Data
+            int number = 1;
+            double doubled = 2.1D;
+            char chara ='a';
+            string name = "irawan";
+            bool status = false;
+
+            Console.WriteLine($"ini number = {number}, ini double = {doubled}, ini char = {chara}, ini string : {name}, ini status = {status}");
+
+            // Konstanta = ada variabel yang tidak dapat diubah
+            const int konstanta = 1;
+            /**
+             * const int konstanta = 1;
+             * konstanta = 2; // error
+             */
+            Console.WriteLine($"ini konstanta : {konstanta}");
+
+            /**
+             * Declare many Variable
+             * 
+             */
+            int x =5, y =10 , z=50;
+            Console.WriteLine($"int {x} , {y} , {z}");
+
+            int h, i, j;
+            h = i = j = 10;
+            Console.WriteLine(h+i+j);
+        }
+    }
+}
